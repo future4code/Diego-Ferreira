f8cb255896bc246381b209c04bf2db964d562eed
Diego Ferreira <diferrsa@gmail.com>
2021-02-22 22:14:19 -0300

    Aula de variáveis

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,46 @@
+/*Exercício de interpretação de código, questão 1. Resposta: 10, 5*/
+/*Exercício de interpretação de código, questão 2. Resposta: 10, 20,10*/
+
+const nome = ''
+const idade = ''
+console.log(typeof [nome, idade])
+
+/*Resposta letra (d) apareceu object, não tem nada declarado nas variáveis*/
+/*Resposta letra (e) notei que no console aparece as variáveis na ordem que as declarei*/
+
+
+Exercício escrita de código 1
+const nomeUsuario = prompt('Qual o seu nome?')
+console.log('nomeUsuario')
+const qualSuaIdade = prompt('Qual a sua idade?')
+console.log('qualSuaIdade')
+console.log('nomeUsuario, qualSuaIdade')
+
+Exercício escrita de código 2
+const seuEndereco = prompt('Qual o seu endereço?')
+console.log(seuEndereco)
+const comidaFavorita = prompt('Qual a sua comida favorita?')
+console.log(comidaFavorita)
+const corFavorita = prompt('Qual a sua cor favorita')
+console.log(corFavorita)
+const timeFavorito = prompt('Qual o seu time favorito')
+console.log(timeFavorito)
+const animalFavorito = prompt('Qual seu animal favorito')
+console.log(animalFavorito)
+
+Exercício escrita de código 3
+const comidaPreferida = ('Feijão, macarrão, frango, Temaki, Yakissoba')
+console.log('Feijão, macarrão, frango, Temaki, Yakissoba')
+console.log('Feijão')
+console.log('macarrão')
+console.log('frango')
+console.log('Temaki')
+console.log('Yakissoba')
+
+
+Exercício escrita de código 4
+let comidaFavorita = ['Feijão, macarrão, frango, Temaki, Yakissoba']
+console.log('Feijão, macarrão, frango, Temaki, Yakissoba')
+console.log(comidaFavorita, [0, 1, 2, 3, 4])
+
+
